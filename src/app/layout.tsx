429c461f1ac684f180a74f4884e350dbdde89ae7
asyncasana <dash.boyeva@gmail.com>
2025-05-27 17:00:57 +0100

    fix: wrap app in SessionProviderWrapper for Next.js 14+

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,10 @@
 import "@/styles/globals.css";
 
 import type { Metadata } from "next";
-import { Geist } from "next/font/google";
-
-import { SessionProvider } from "next-auth/react";
-
+import { Inter } from "next/font/google";
 import Header from "./_components/Header";
 import Footer from "./_components/Footer";
+import SessionProviderWrapper from "./_components/SessionProviderWrapper";
 
 export const metadata: Metadata = {
   title: "Hair with Iryna",
@@ -14,22 +12,22 @@ export const metadata: Metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
-const geist = Geist({
+const inter = Inter({
   subsets: ["latin"],
-  variable: "--font-geist-sans",
+  variable: "--font-inter",
 });
 
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${geist.variable}`}>
+    <html lang="en" className={`${inter.variable}`}>
       <body>
-        <SessionProvider>
-            <Header />
-            {children}
-            <Footer />
-        </SessionProvider>
+        <SessionProviderWrapper>
+          <Header />
+          {children}
+          <Footer />
+        </SessionProviderWrapper>
       </body>
     </html>
   );
